1f1d5c78c4c9dd9ef8af5f2d1c280de035c32415
Aline <alinea@riseup.net>
2016-11-30 14:25:20 +0100

    Bluetooth thread now properly reconnects to a device that has been disconnected

--- code/app/src/main/java/verteiltesysteme/penguard/guardianservice/BluetoothThread.java
+++ code/app/src/main/java/verteiltesysteme/penguard/guardianservice/BluetoothThread.java
@@ -1,5 +1,9 @@
 package verteiltesysteme.penguard.guardianservice;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothManager;
+import android.bluetooth.BluetoothProfile;
+import android.content.Context;
 import android.util.Log;
 
 import java.util.Vector;
@@ -8,20 +12,30 @@ class BluetoothThread extends Thread {
 
     private final Vector<Penguin> penguins;
     private GuardService service;
+    private BluetoothManager manager;
 
     BluetoothThread(final Vector<Penguin> penguins, GuardService service) {
         this.penguins = penguins;
         this.service = service;
+        this.manager = (BluetoothManager) service.getSystemService(Context.BLUETOOTH_SERVICE);
     }
 
     public void run() {
         while (!isInterrupted()) {
+
+            //TODO this crashes hard if the user randomly disables bluetooth
             for (Penguin p : penguins) {
-                debug("Initiating RSSI scan for penguin " + p.getName());
                 if (p.getGatt() == null) {
                     p.setGatt(p.getDevice().connectGatt(service, true, p.bluetoothGattCallback));
                 }
-                p.getGatt().readRemoteRssi();
+                else if (manager.getConnectionState(p.getDevice(), BluetoothProfile.GATT) != BluetoothAdapter.STATE_CONNECTED) {
+                    debug("Initiating connect for penguin " + p.getName());
+                    p.getGatt().connect();
+                }
+                else {
+                    debug("Initiating RSSI scan for penguin " + p.getName());
+                    p.getGatt().readRemoteRssi();
+                }
             }
 
             try {
