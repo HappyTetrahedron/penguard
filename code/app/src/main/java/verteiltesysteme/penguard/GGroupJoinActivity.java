eea86c7cf6efacaf96110171ea3847411f5aff13
Nicole Thurnherr <nicoleth@student.ethz.ch>
2016-12-05 11:01:48 +0100

    #17 done basic UI for that

--- code/app/src/main/java/verteiltesysteme/penguard/GGroupJoinActivity.java
+++ code/app/src/main/java/verteiltesysteme/penguard/GGroupJoinActivity.java
@@ -6,15 +6,23 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
 
 //TODO use this activity to initiate group join by binding to service and calling respective method in there, see issue #17
 
 public class GGroupJoinActivity extends AppCompatActivity {
 
+    Button btn;
+    EditText editText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ggroup_join);
+
+        btn = (Button)findViewById(R.id.groupJoinBT);
+        editText = (EditText)findViewById(R.id.groupJoinET);
     }
 
     @Override
