8ee5a96a526bf492b0f9cc6017939b4a37af070a
Luca Wolf <lwolf@student.ethz.ch>
2016-12-08 17:29:27 +0100

    server should works as intended

--- actualServer/server.py
+++ actualServer/server.py
@@ -7,12 +7,11 @@ from server_protocol import PenguinServerProtocol
 def start_server(port):
     # Fancy python 3.5 stuff for async stuff
     loop = asyncio.get_event_loop()
-    ip = get_ip_address("eth0")
-    print("Starting UDP server on %s:%d" % (ip, port))
+    print("Starting UDP server on port: %d" % port)
     # One protocol instance will be created to serve all client requests
     listen = loop.create_datagram_endpoint(
         PenguinServerProtocol,
-        local_addr=(get_ip_address("eth0"), port))
+        local_addr=('0.0.0.0', port))
     transport, protocol = loop.run_until_complete(listen)
     
     try:
@@ -23,10 +22,3 @@ def start_server(port):
     transport.close()
     loop.close()
 
-def get_ip_address(ifname):
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    return socket.inet_ntoa(fcntl.ioctl(
-        s.fileno(),
-        0x8915,  # SIOCGIFADDR
-        struct.pack('256s', bytes(ifname[:15], "UTF-8"))
-    )[20:24])
--- actualServer/server_protocol.py
+++ actualServer/server_protocol.py
@@ -39,56 +39,143 @@ class PenguinServerProtocol:
         return msg
 
     def send(self, msg, addr):
+        # serialize message
         serialized_message = msg.SerializeToString()
         msg_length = len(serialized_message)
         msg_length = encoder._VarintBytes(msg_length)
+
+        # the actual message sent is (len(msg) ++ msg)
         final_message = msg_length + serialized_message
         self.transport.sendto(msg_length + serialized_message, addr)
-        
 
 
     # =================== HANDLERS ===================
     def default_handler(self, msg, addr):
-        response = PGPMessage()
-        response.type=PGPMessage.SG_ERR
-        response.error.error = 'No handler found for this message type.'
-        send(response, addr)
+        self.send_err('No handler found for this message type.', addr)
+
 
     def register_client(self, msg, addr):
         table = self.db['guardians']
         if table.find_one(name=msg.name):
-            response = PGPMessage()
-            response.type=PGPMessage.SG_ERR
-            response.error.error = 'User with that name already exists.'
-            self.send(response, addr)
+            # Name not available -> send error message
+            self.send_err('Name is not available.', addr)
+
         else:
             # create unique uuid
-            unique_uuid = False
-            while not unique_uuid:
-                new_uuid = str(uuid.uuid4())
-                if not table.find_one(uuid=new_uuid):
-                    unique_uuid = True
-                    new_uuid = str(new_uuid)
+            new_uuid = self.create_uuid(table)
 
             # store that stuff
             table.insert(dict(name=msg.name, uuid=new_uuid, ip=addr[0], port=addr[1]))
 
             # build response
-            response = PGPMessage()
-            response.type=PGPMessage.SG_ACK
-            response.ack.uuid = new_uuid
-            response.ack.ip = addr[0]
-            response.ack.port = addr[1]
-
-            # send
-            self.send(response, addr)
+            self.send_ack(new_uuid, addr, name=msg.name)
 
 
     def deregister_client(self, msg, addr):
-        default_handler(msg, addr)
+        table = self.db['guardians']
+        if table.find_one(uuid=msg.goodbye.uuid):
+            # delete from table
+            table.delete(uuid=msg.goodbye.uuid)
+
+            # respond with ack
+            self.send_ack(msg.goodbye.uuid, addr)
+
+        else:
+            # user not found -> send error
+            self.send_err('User with this uuid has not been found.', addr)
+
 
     def handle_ping(self, msg, addr):
-        default_handler(msg, addr)
+        table = self.db['guardians']
+
+        # find guardian in table
+        guardian = table.find_one(uuid=msg.ping.uuid)
+
+        # a response will be sent anyway
+        if guardian:
+            if self.guardian_changed(guardian, msg, addr):
+                self.update_guardian(table, guardian, msg, addr)
+
+            # send ack to client
+            self.send_ack(msg.ping.uuid, addr, name=msg.name)
+
+        else:
+            # user not found -> response is error message
+            self.send_err('User with this uuid has not been found', addr)
+        
 
     def group_request(self, msg, addr):
-        default_handler(msg, addr)
+        table = self.db['guardians']
+        guardian = table.find_one(name=msg.groupReq.name)
+
+        if guardian:
+            # send ack to the request guardian
+            self.send_ack(msg.ping.uuid, addr, name=msg.name)
+
+            # build MergeReq message
+            response = PGPMessage()
+            response.type=PGPMessage.SG_MERGE_REQ
+            response.mergeReq.name = msg.name
+            response.mergeReq.ip = addr[0]
+            response.mergeReq.port = addr[1]
+
+            # send to the requested guardian
+            self.send(response, (guardian['ip'], guardian['port']))
+
+            print('Send merge request')
+
+        else:
+            self.send_err('User with this name has not been found.', addr)
+
+
+    # ============== helpers =================
+    def send_ack(self, uuid, addr, name=None):
+        print('Sent message to %s:%d, message_type=ack' % (addr[0], addr[1]))
+        # build ack
+        response = PGPMessage()
+
+        response.type = PGPMessage.SG_ACK
+        response.ack.uuid = uuid
+        response.ack.ip = addr[0]
+        response.ack.port = addr[1]
+        if name:
+            response.ack.name = name
+
+        # actually send it
+        self.send(response, addr)
+
+    def send_err(self, error_message, addr):
+        print('Sent message to %s:%d, message_type=error, message: %s' % (addr[0], addr[1], error_message))
+        # build protobuf
+        response = PGPMessage()
+        response.type=PGPMessage.SG_ERR
+        response.error.error = error_message
+
+        # send it
+        self.send(response, addr)
+
+
+    def create_uuid(self, table):
+        # create new uuids until we have one that doesen't exist yet
+        unique_uuid = False
+        while not unique_uuid:
+            new_uuid = str(uuid.uuid4())
+            if not table.find_one(uuid=new_uuid):
+                unique_uuid = True
+
+        return new_uuid
+
+
+    def guardian_changed(self, guardian, msg, addr):
+        name_changed = guardian['name'] == msg.name
+        ip_changed = guardian['ip'] == addr[0]
+        port_changed = guardian['port'] = addr[1]
+
+        return name_changed or ip_changed or port_changed
+
+    def update_guardian(self, table, guardian, msg, addr):
+        guardian['name'] = msg.name
+        guardian['ip'] = addr[0]
+        guardian['port'] = addr[1]
+        table.update(guardian, ['uuid'])
+
