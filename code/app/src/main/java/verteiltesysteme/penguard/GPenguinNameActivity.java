7d30138f95007af5102b2d95fc0d2da635090d25
Nicole Thurnherr <nicoleth@student.ethz.ch>
2016-12-09 10:52:12 +0100

    PenguinNameActivity: design done, removed magic strings, replaced log method with our debug, replaced the 4 lines for toast with our toast method and added access to the settings

--- code/app/src/main/java/verteiltesysteme/penguard/GPenguinNameActivity.java
+++ code/app/src/main/java/verteiltesysteme/penguard/GPenguinNameActivity.java
@@ -7,49 +7,75 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class GPenguinNameActivity extends AppCompatActivity {
-    private final String TAG = "##GPenguinNameAct##";
     private  BluetoothDevice device;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gpenguin_name);
-        Log.d(TAG, "onCreate: entered nameingphase");
-        TextView setName = (TextView) findViewById(R.id.textView2);
-        setName.setText("How do you want to name your penguin?");
+        debug("onCreate: entered nameingphase");
         Intent intent = getIntent();
         device = intent.getParcelableExtra("device");
     }
 
     public void submit(View view){
-        Log.d(TAG, "submit: button was clicked");
+        debug("submit: button was clicked");
         EditText newName = (EditText) findViewById(R.id.newPenguinName);
         String newPenguinName = newName.getText().toString();
 
         if (!newPenguinName.equals("")) {
-            Log.d(TAG, "submit: textedit was not empty");
+            debug( "submit: textedit was not empty");
             Intent resultData = new Intent();
             resultData.putExtra("device", device);
             resultData.putExtra("newName", newPenguinName);
-            Log.d(TAG, "submit: setup result");
+            debug( "submit: setup result");
             setResult(Activity.RESULT_OK, resultData);
 
             Context context = getApplicationContext();
-            CharSequence text = "Penguin was successfully named";
-            int duration = Toast.LENGTH_SHORT;
-            Toast.makeText(context, text, duration).show();
+            toast(getString(R.string.penguinName_suc));
 
             finish();
         } else{
-            Context context = getApplicationContext();
-            CharSequence text = "Please choose a name for your penguin";
-            int duration = Toast.LENGTH_SHORT;
-            Toast.makeText(context, text, duration).show();
+            toast(getString(R.string.penguinName_empty));
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_settings, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.menu_settings:
+                Intent intent = new Intent(this, SettingsActivity.class);
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    private void toast(String msg){
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+    }
+
+
+    private void debug(String msg) {
+        Log.d("PenguinNameActivity", msg);
     }
 }
