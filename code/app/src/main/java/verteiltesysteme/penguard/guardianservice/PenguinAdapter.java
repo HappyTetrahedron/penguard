e1a8a924d66c198a027bc348625fbd09fc5b3d37
Nicole Thurnherr <nicoleth@student.ethz.ch>
2016-12-15 19:26:44 +0100

    penguin layout closes #49

--- code/app/src/main/java/verteiltesysteme/penguard/guardianservice/PenguinAdapter.java
+++ code/app/src/main/java/verteiltesysteme/penguard/guardianservice/PenguinAdapter.java
@@ -1,11 +1,14 @@
 package verteiltesysteme.penguard.guardianservice;
 
 import android.content.Context;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.support.annotation.NonNull;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.List;
@@ -32,10 +35,28 @@ class PenguinAdapter extends ArrayAdapter<Penguin> {
         Penguin penguin = getItem(position);
 
         if (penguin != null) {
-            TextView titleTextView = (TextView) convertView.findViewById(R.id.penguinName);
+            TextView penguinName = (TextView) convertView.findViewById(R.id.penguinName);
+            TextView statusTV = (TextView) convertView.findViewById(R.id.PenguinStatus);
+            ImageView icon = (ImageView)convertView.findViewById(R.id.imageView);
+            icon.setImageResource(R.drawable.icon);
 
-            if (titleTextView != null) {
-                titleTextView.setText(penguin.getName() + " " + penguin.isSeen());
+            penguinName.setText(penguin.getName());
+
+            if (penguinName != null) {
+
+                if (penguin.isSeen()){ //seen by myself
+                    //icon.setBackgroundColor(Color.GREEN);
+                    icon.setImageTintList(ColorStateList.valueOf(Color.GREEN));
+                    statusTV.setText("is seen by you");
+                }else if (penguin.isSeenByAnyone() && !penguin.isSeen()){
+                    statusTV.setText("seen by someone else");
+                    icon.setImageTintList(ColorStateList.valueOf(Color.BLUE));
+                }else {
+                    statusTV.setText("not seen by anyone");
+                   //icon.setBackgroundColor(Color.RED);
+                    icon.setImageTintList(ColorStateList.valueOf(Color.RED));
+
+                }
             }
         }
 
