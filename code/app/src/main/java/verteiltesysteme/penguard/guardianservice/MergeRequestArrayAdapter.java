6288df064f8eeec9cc7eb7878f6afd8fb9f49b07
Aline <alinea@riseup.net>
2016-12-18 12:20:54 +0100

    Fixed icon coloring in MergeRequestAdapter

--- code/app/src/main/java/verteiltesysteme/penguard/guardianservice/MergeRequestArrayAdapter.java
+++ code/app/src/main/java/verteiltesysteme/penguard/guardianservice/MergeRequestArrayAdapter.java
@@ -1,11 +1,13 @@
 package verteiltesysteme.penguard.guardianservice;
 
 import android.content.Context;
+import android.content.res.ColorStateList;
 import android.support.annotation.NonNull;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -36,6 +38,8 @@ public class MergeRequestArrayAdapter extends ArrayAdapter<PenguardProto.PGPMess
 
 
         if (request != null) {
+            ImageView icon = (ImageView)convertView.findViewById(R.id.imageView);
+            icon.setImageTintList(ColorStateList.valueOf(0xff2f2f2f)); //2f2f2f
             TextView titleTextView = (TextView) convertView.findViewById(R.id.guardianName);
 
             if (titleTextView != null) {
